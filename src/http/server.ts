c543b9bdeee0b310f19eb767c601db1d4e116215
gpmarchi <gustavomarchi@gmail.com>
2024-02-07 12:11:19 -0300

    feat: add fastify cookie and register plugin

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,14 @@
+import cookie from '@fastify/cookie';
 import fastify from 'fastify';
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
 
 const app = fastify()
+
+app.register(cookie, {
+  secret: 'polls-app-nlw',
+  hook: 'onRequest',
+})
 
 app.register(createPoll)
 app.register(getPoll)
